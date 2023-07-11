70f4d789463009b809aa72ad2501d4bbd955c5a8
Ninah11 <ninahmut@gmail.com>
2023-07-11 06:12:01 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ count = 0;
 		return (1);
 	}
 	/**
-	 * main - Print the name of the program
+	 *main - Print the name of the program
 	 * @argc: Count arguments
 	 * @argv: Arguments
 	 * Return: Always 0 (Success)
