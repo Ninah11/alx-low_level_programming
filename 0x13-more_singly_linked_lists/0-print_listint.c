037b14c08b77216d54149dbd589b95116d448188
Ninah11 <ninahmut@gmail.com>
2023-08-01 14:08:57 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -19,27 +19,3 @@ h = h->next;
 
 return (num);
 }
-
-1-listint_len.c
-
-#include "lists.h"
-
-/**
- * listint_len - Returns the number of elements in a linked list.
- * @h: Pointer to the linked list of type listint_t to traverse.
- *
- * Return: The number of nodes in the linked list.
- */
-size_t listint_len(const listint_t *h)
-{
-size_t num = 0;
-
-while (h)
-{
-num++;
-h = h->next;
-}
-
-return (num);
-}
-
